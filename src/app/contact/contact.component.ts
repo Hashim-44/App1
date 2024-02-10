24518b454ed2273395185c165c871c0b13b5cc79
Hashim <hashim.huawei2001@gmail.com>
2024-02-10 23:16:09 +0200

    upload

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent {
+// mylable(element: HTMLLabelElement):void{
+//   element.classList.add('top-0');
+//   element.classList.remove('.top-0-top')
+  
+// }
+
+name:string=''
+age:string=''
+mail:string=''
+password:string=''
+
+}
